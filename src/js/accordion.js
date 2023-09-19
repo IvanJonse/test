fcbf0b8005e92e649d3cc3fdf46a86497f1fbfa4
IvanJonse <kgiv31777@gmail.com>
2023-09-19 18:29:17 +0700

    fix bugs

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,4 +1,5 @@
 const accordion = document.querySelectorAll('.accordion__item')
+const accordionWrap = document.querySelectorAll('.accordion__item__wrap')
 const btnAccordion = document.querySelectorAll('.accordion__item__btn')
 const titleAccordion = document.querySelectorAll('.accordion__item__title')
 const textAccordion = document.querySelectorAll('.accordion__item__text')
@@ -7,7 +8,7 @@ function addClass(div, title) {
     div.classList.toggle(title)
 }
 
-accordion.forEach((el, i) => el.addEventListener('click', ()=> {
+accordionWrap.forEach((el, i) => el.addEventListener('click', ()=> {
     addClass(accordion[i], 'accordion__item__active')
     addClass(titleAccordion[i], 'accordion__item__title__active')
     addClass(btnAccordion[i], 'accordion__item__btn__active')
--- src/js/header.js
+++ src/js/header.js
@@ -14,13 +14,11 @@ const menu = document.getElementById("App__isOpen");
 
 menuToggle.addEventListener("click", function () {
         menu.style.display = 'block'
-        menu.style.transform = 'translateX(0%)'
         menuToggle.style.display = 'none'
     }
 )
 
 closeIcon.addEventListener("click", function () {
-        menu.style.transform = 'translateX(100%)'
         menuToggle.style.display = 'flex'
         menu.style.display = 'none'
     }
