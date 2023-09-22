54d383a4b18821134310fc7d8600bea80feec71c
IvanJonse <kgiv31777@gmail.com>
2023-09-22 15:53:35 +0700

    fix bugs

--- src/js/sideMenu.js
+++ src/js/sideMenu.js
@@ -3,16 +3,29 @@ const menuItems = document.querySelectorAll(".sideMenu__list__item");
 const menuLink = document.querySelectorAll(".sideMenu__list__item__link");
 const sections = document.querySelectorAll("section")
 
-window.addEventListener("load", function() {
-    
-    const options = {
-      threshold: 0.18
-    }
 
+const calcWidth = () => {
+} 
+window.addEventListener("load", function() {
+
+    const options = {
+      threshold: 
+      window.innerWidth < 1155 ?  0.2 
+      : window.innerWidth < 1255 ?  0.25  
+      : window.innerWidth < 1555 ?  0.27 
+      :  window.innerWidth <= 1800 ? 0.35
+       : 0.38,
+      rootMargin:  window.innerWidth < 1555 ? '-10px' 
+      : window.innerWidth <= 1740 ? '-60px' 
+      : window.innerWidth >= 1800 ?  '-110px'
+       : '-60px' 
+
+    }
+console.log(options.threshold);
     let observer = new IntersectionObserver(entries => {
       entries.forEach((entry, i) => {
         const targetId =  entry.target.getAttribute('id')
-                  
+             
         if (entry.isIntersecting) {
             menuLink.forEach(e=> e.classList.remove('active__item'))
             this.document.querySelector(`[href='#${targetId}']`)
@@ -35,7 +48,7 @@ menuItems.forEach((item, i) => {
         const targetSection = document.getElementById(targetId);
 
         app.scrollTo({
-            top: targetSection.offsetTop - 170,
+            top: targetSection.offsetTop - 100,
             left: 0,
             behavior: "smooth"
         });
